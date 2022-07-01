60582d1b5bc581241f46fb22713b3ad3d0a79efd
Caio Cesar <caiolopesbarros@hotmail.com>
2022-07-01 15:05:55 +0200

    seeds

--- src/models/mongo/connect.js
+++ src/models/mongo/connect.js
@@ -1,17 +1,23 @@
 import * as dotenv from "dotenv";
 import Mongoose from "mongoose";
 
-/* import * as seeder from "mais-mongoose-seeder";
-import { seedData } from "./seed-data.js";
+import * as seeder from "mais-mongoose-seeder";
+import { userSeed } from "./seeds/user-seeds.js";
+import { postSeed } from "./seeds/post-seeds.js";
 
 const seedLib = seeder.default;
 
 async function seed() {
   const seedObj = seedLib(Mongoose);
-  const dbData = await seedObj.seed(seedData, { dropDatabase: false, dropCollections: true });
-  console.log(dbData);
-} */
+  const dbDataUsers = await seedObj.seed(userSeed, { dropDatabase: false, dropCollections: true });
+  
+  postSeed.posts.post1.userId = dbDataUsers.users.caio._id;
+  postSeed.posts.post2.userId = dbDataUsers.users.caio._id;
+  postSeed.posts.post3.userId = dbDataUsers.users.alan._id;
+  postSeed.posts.post4.userId = dbDataUsers.users.diego._id;
+  const dbDataPosts = await seedObj.seed(postSeed, { dropDatabase: false, dropCollections: true });
 
+}
 export function connectMongo() {
   dotenv.config();
 
@@ -28,6 +34,6 @@ export function connectMongo() {
 
   db.once("open", function () {
     console.log(`database connected to ${this.name} on ${this.host}`);
-    //seed()
+    seed()
   });
 }
---
+++ src/models/mongo/seeds/user-seeds.js
@@ -0,0 +1,34 @@
+export const userSeed = {
+    users: {
+      _model: "User",
+      admin: {
+        name: "Admin",
+        picture: "https://thumbs.dreamstime.com/b/admin-zeichen-auf-dem-notebook-symbol-vektor-laptop-lagervektor-166205404.jpg",
+        email: "admin@admin.com",
+        password: "admin123",
+        admin: true
+      },
+      caio: {
+        name: "Caio Cesar",
+        picture: "https://media-exp1.licdn.com/dms/image/C4D03AQEJJXfrtgBSCQ/profile-displayphoto-shrink_200_200/0/1647538264826?e=1660176000&v=beta&t=EFBYyZBL7YvqFSRh4iQ-sc6q7Q25abIqDwusLPjf7Zs",
+        email: "CaioCesar@email.com",
+        password: "Caio123",
+        admin: false
+      },
+      alan: {
+        name: "Alan Piana",
+        picture: "https://extra.globo.com/incoming/25077364-723-9b7/w367h550-PROP/supla11.JPG",
+        email: "AlanPiana@email.com",
+        password: "Alanl123",
+        admin: false
+      },
+      diego: {
+        name: "Diego Cruz",
+        picture: "https://portalpepper.com.br/wp-content/uploads/2020/12/Joao_Suplicy_-_Coronavirus.jpg",
+        email: "DiegoCruz@email.com",
+        password: "Diego123",
+        admin: false
+      },
+    },
+  };
+  
